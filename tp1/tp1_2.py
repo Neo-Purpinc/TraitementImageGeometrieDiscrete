714fd53e71e44475b54a012d68bf3432228e1c22
Neo <neo.purpinc@gmail.com>
2022-11-03 16:03:45 +0100

    Fin Exercice 2

--- tp1/tp1_2.py
+++ tp1/tp1_2.py
@@ -3,15 +3,6 @@ import matplotlib.pyplot as plt
 from matplotlib import patches
 from skimage import (io, morphology)
 
-def carre(x:int):
-    return np.ones((x, x), dtype=np.uint8)
-
-def rectangle(x:int, y:int):
-    return np.ones((x, y), dtype=np.uint8)
-
-def erosion(img:np.ndarray, struct:np.ndarray):
-    return 1-dilatation(1-img, struct)
-
 def dilatation(img:np.ndarray, struct:np.ndarray):
     res = np.zeros(img.shape, dtype=np.uint8)
     padded = np.pad(img, tuple(item//2 for item in struct.shape), mode='constant', constant_values=0)
@@ -20,33 +11,29 @@ def dilatation(img:np.ndarray, struct:np.ndarray):
                 res[i, j] = np.max(padded[i:i+struct.shape[0], j:j+struct.shape[1]]*struct)
     return res
 
-
-def ouverture(img:np.ndarray, struct:np.ndarray):
-    return dilatation(erosion(img, struct), struct)
-
-def fermeture(img:np.ndarray, struct:np.ndarray):
-    return erosion(dilatation(img, struct), struct)
-
 def test():
     struct = np.array([
-        [1,0,0,0,0],
-        [0,1,0,0,0],
-        [0,0,1,0,0],
-        [0,0,0,1,0],
-        [0,0,0,0,1]
+        [1,0,0],
+        [0,1,0],
+        [0,0,1]
     ])
-    struct = carre(5)
     img_test = io.imread('./tp1/lines.png', as_gray=True)
-    fig, ax = plt.subplots(1, 3, figsize=(10, 10))
+    fig, ax = plt.subplots(1, 4, figsize=(10, 10))
     ax[0].imshow(img_test, cmap='gray')
-    ax[0].set_title('Image de base')
-    for i in range(1):
-        img_test = fermeture(img_test, struct)
-    ax[1].imshow(img_test, cmap='gray')
-    ax[1].set_title('Suppression des diagonales')
-    ax[2].imshow(morphology.closing(img_test, struct), cmap='gray')
-    ax[2].set_title('Version SCKIT')
-    for i in range(3):
+    ax[0].set_title('Image de base (Grayscale)')
+    #img_test to binary
+    img_test = (img_test > 0.5).astype(np.uint8)
+    
+    res_scikit = np.copy(img_test)
+    res = np.copy(img_test)
+    for i in range(4):
+        res = dilatation(res, struct)
+        res_scikit = morphology.binary_dilation(res_scikit, struct)
+    ax[2].imshow(res, cmap='gray')
+    ax[2].set_title('Suppression des diagonales')
+    ax[3].imshow(res_scikit, cmap='gray')
+    ax[3].set_title('Version SCKIT')
+    for i in range(4):
         ax[i].set_axis_off()
     plt.show()
 test()
